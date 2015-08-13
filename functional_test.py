27d412cfb4d2e2d753ec051130cb934194233a34
Stefania <bruschi@usc.edu>
2015-08-13 15:42:45 -0700

    Refactor home page view to use a template

---
+++ functional_test.py
@@ -0,0 +1,64 @@
+from selenium import webdriver
+import unittest
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        # open firefox browser on selenium hub
+        selenium_hub_url = 'http://192.168.99.100:4444/wd/hub'
+        self.browser = webdriver.Remote(
+                                   command_executor=selenium_hub_url,
+                                   desired_capabilities={"browserName": "firefox"}
+                                   )
+        self.django_url = 'http://192.168.99.100:8000'
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):          
+        self.browser.get(self.django_url)
+        
+        # She notices the page title and header mention to-do lists
+        self.assertIn ('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_id('h1').text()
+        self.assertIn('To-Do', header_text )
+        
+        
+        # She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item', msg)
+        
+        # She types "Buy peacock feathers" into a text box (Edith's hobby
+        # is tying fly-fishing lures)
+        inputbox.send_keys('Buy peacock feathers')
+        
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list
+        inputbox.send_keys(Keys.ENTER)
+        
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
+        
+        # There is still a text box inviting her to add another item. She
+        # enters "Use peacock feathers to make a fly" (Edith is very methodical)
+        
+        self.fail('Finish the test!')
+        
+        # The page updates again, and now shows both items on her list
+        
+        # Edith wonders whether the site will remember her list. Then she sees
+        # that the site has generated a unique URL for her -- there is some
+        # explanatory text to that effect.
+        
+        # She visits that URL - her to-do list is still there.
+        
+        # Satisfied, she goes back to sleep
+        
+        browser.quit()
+        
+if __name__ == '__main__':  #7
+    unittest.main(warnings='ignore')  #8
