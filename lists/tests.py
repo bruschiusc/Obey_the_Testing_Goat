e219b64b9d4580def26ce27cafcffd1903e80a34
Stefania <bruschi@usc.edu>
2015-08-13 17:07:13 -0700

    before refactory chapert 5

--- lists/tests.py
+++ lists/tests.py
@@ -14,8 +14,20 @@ class HomePageTest (TestCase):
     def test_home_page_return_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        #self.assertTrue(response.content.startswith(b'<html>'))
-        #self.assertIn(b'<title>To-Do lists</title>', response.content)
-        #elf.assertTrue(response.content.endswith(b'</html>'))
         expect_html = render_to_string('home.html')
-        self.assertEqual(response.content.decore(), expect_html)
+        self.assertEqual(response.content.decode(), expect_html)
+        
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+        
+        response = home_page(request)
+           
+        
+        self.assertIn('A new list item', response.content.decode())
+        expected_html = render_to_string(
+            'home.html',
+            {'new_item_text':  'A new list item'}
+        )
+        self.assertEqual(response.content.decode(), expected_html)
