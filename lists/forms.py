20c14b3ee7956d09b1de112d86fa3fc9af6adb3e
Stefania <bruschi@usc.edu>
2015-08-20 11:11:14 -0700

    Create Form

---
+++ lists/forms.py
@@ -0,0 +1,14 @@
+'''
+Created on Aug 20, 2015
+
+@author: stefania
+'''
+from django import forms
+
+class ItemForm(forms.Form):
+    item_text = forms.CharField(
+            widget = forms.fields.TextInput(attrs={
+                 'placeholder': "Enter a to-do item",
+                 'class': 'form-control input-lg',
+                })
+    )
