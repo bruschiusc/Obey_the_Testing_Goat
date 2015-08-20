aa963c8247031f5dbee29db646b411df6370c718
Stefania <bruschi@usc.edu>
2015-08-20 16:11:03 -0700

    Add the check of unique item in the list. FT are broken

--- lists/models.py
+++ lists/models.py
@@ -11,7 +11,7 @@ class List(models.Model):
 
 
 class Item(models.Model):
-    text = models.TextField(default='')
+    text = models.TextField(default='', unique=True)
     list = models.ForeignKey(List, default=None)
     
     
