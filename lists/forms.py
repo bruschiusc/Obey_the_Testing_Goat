e0e7a9e89cdc1dcd19392dea366d79d8ef489e5b
Stefania <bruschi@usc.edu>
2015-08-20 18:04:03 -0700

    Andvance Form with unique contrains in the database in item and text

--- lists/forms.py
+++ lists/forms.py
@@ -5,6 +5,7 @@ Created on Aug 20, 2015
 '''
 from django import forms
 from lists.models import Item
+from django.core.exceptions import ValidationError
 
 EMPTY_ITEM_ERROR = "You can't have an empty list item"
 DUPLICATE_ITEM_ERROR = "You've already got this in your list"
@@ -40,6 +41,7 @@ class ItemForm(forms.models.ModelForm):
 class ExistingListItemForm(ItemForm):
     def __init__(self, for_list, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.instance.list = for_list
         
     def validate_unique(self):
         try:
@@ -47,5 +49,7 @@ class ExistingListItemForm(ItemForm):
         except ValidationError as e:
             e.error_dict = {'text': [DUPLICATE_ITEM_ERROR]}
             self._update_errors(e)
-        
+    
+    def save(self):
+        return forms.models.ModelForm.save(self)
     
